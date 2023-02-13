593f327ff0b0d772045d04f6c3cc8befe3de35e1
Ayaan Siddiqui <ars394@pitt.edu>
2023-02-13 16:35:22 -0500

    Initial Commit

---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by Ayaan Rushil Siddiqui on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
